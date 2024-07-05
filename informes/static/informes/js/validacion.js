aadd2c5a49ee0e1f629afd4e34386afedf201bab
Felipe Echeverria <fe.echeverrias@duocuc.cl>
2024-07-05 13:49:36 -0400

    carga de archivos proyecto 2

---
+++ informes/static/informes/js/validacion.js
@@ -0,0 +1,23 @@
+// Validación del formulario usando Bootstrap
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        var forms = document.getElementsByClassName('needs-validation');
+        Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+function submitForm() {
+    var form = document.getElementById('informeForm');
+    if (form.checkValidity()) {
+        form.submit();
+    }
+}
